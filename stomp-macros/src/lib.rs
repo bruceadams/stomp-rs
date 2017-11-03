382a19f02e964ae6afc30a5791206f99ad67951f
Bruce Adams <bruce.adams@acm.org>
2017-11-03 18:33:48 -0400

    Only output our expanded code

--- stomp-macros/src/lib.rs
+++ stomp-macros/src/lib.rs
@@ -19,12 +19,12 @@ pub fn stomp_command(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
     let expanded = stomp_command::expand(&ast, &attrs, &field_attrs);
     attrs.check_used(ast.ident.as_ref(), None);
     field_attrs.check_used(ast.ident.as_ref());
-    quote!(#ast #expanded).to_string().parse().unwrap()
+    quote!(#expanded).to_string().parse().unwrap()
 }
 
 #[proc_macro_derive(StompCommands)]
 pub fn stomp_commands(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse_macro_input(&input.to_string()).unwrap();
     let expanded = stomp_commands::expand(&ast);
-    quote!(#ast #expanded).to_string().parse().unwrap()
+    quote!(#expanded).to_string().parse().unwrap()
 }
